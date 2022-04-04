203c89a84e1ca9ad640ee6b9277c51afc9c2bbf0
pascalbaljet <pascal@pascalbaljet.nl>
2022-04-04 16:26:46 +0200

    Checkbox/Radio slots

--- src/form-components-pro-vue3-tailwind3-simple/src/Radio.vue
+++ src/form-components-pro-vue3-tailwind3-simple/src/Radio.vue
@@ -15,11 +15,15 @@
           text-indigo-600
           shadow-sm
           focus:border-indigo-300
-          focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+          focus:ring
+          focus:ring-indigo-200
+          focus:ring-opacity-50
         "
       />
 
-      <span class="ml-2">{{ label }}</span>
+      <span class="ml-2"
+        ><slot>{{ label }}</slot></span
+      >
     </label>
 
     <Error v-if="errorMessage" :message="errorMessage" />
